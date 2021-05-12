fc8f7bbc05bb6537ce63c83d8bdc9a4821901e22
Nina <259290@pwr.edu.pl>
2021-05-12 13:29:02 +0200

    testy wektora 3d

--- tests/main.cpp
+++ tests/main.cpp
@@ -1,5 +1,128 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
+#include "../tests/doctest/doctest.h"
+#include "../include/vector.hh"
+#include "../include/Cuboid.hh"
+#include "../include/matrix.hh"
+#include "../include/size.hh"
 
-// This is all that is needed to compile a test-runner executable.
-// More tests can be added here, or in a new tests/*.cpp file.
+
+TEST_CASE("konstruktor parametryczny")
+{
+    double a = 1.0, b = 2.0, c=3.0 ;
+    Vector3D wektor  /*( a, b )*/  ;
+
+    wektor[0] = a;
+    wektor[1] =b;
+    wektor[2] =c;
+
+    CHECK(wektor[0] == 1);
+    CHECK(wektor[1] == 2);
+    CHECK(wektor[2] == 3);
+}
+
+
+TEST_CASE("konstruktor bezparametryczny")
+{
+
+    Vector3D wektor;
+
+    CHECK(wektor[0] == 0);
+    CHECK(wektor[1] == 0);
+    CHECK(wektor[2] == 0);
+}
+
+TEST_CASE("Dodawanie wektorów")
+{
+    double tab1[3]={1,2,3};
+    double tab2[3]={2,1,3};
+    Vector3D wektor1(tab1);
+    Vector3D wektor2(tab2);
+    Vector3D wynik;
+
+    wynik = wektor1 + wektor2;
+    CHECK(wynik[0] == 3);
+    CHECK(wynik[1] == 3);
+    CHECK(wynik[2] == 6);
+}
+
+TEST_CASE("Odejmowanie wektorów")
+{
+    double tab1[3]={1,2,3};
+    double tab2[3]={2,1,3};
+    Vector3D wektor1(tab1);
+    Vector3D wektor2(tab2);
+    Vector3D wynik;
+
+    wynik = wektor1 - wektor2;
+    CHECK(wynik[0] == -1);
+    CHECK(wynik[1] == 1);
+    CHECK(wynik[2] == 0);
+}
+
+TEST_CASE("wektor * stała")
+{
+    double tab1[3]={1,2,3};
+    Vector3D wektor1(tab1);
+    double a = 2;
+    Vector3D wynik;
+
+    wynik = wektor1 * a;
+    CHECK(wynik[0] == 2);
+    CHECK(wynik[1] == 4);
+    CHECK(wynik[2] == 6);
+}
+
+TEST_CASE("wektor - wyświetlanie")
+{
+    double tab1[3]={1,2,3};
+    Vector3D wektor1(tab1);
+    // double a[2]={1,2};
+    std::ostringstream StrmWyj;
+    StrmWyj << wektor1;
+    std::cout << StrmWyj.str();
+    CHECK("1 2 3 " == StrmWyj.str());
+}
+
+TEST_CASE("wektor - wpisywanie")
+{
+    Vector3D a;
+
+    std::istringstream in("1 1 1");
+    in >> a;
+    std::ostringstream out;
+    out << a;
+
+    CHECK("1 1 1 " == out.str());
+}
+
+TEST_CASE("operator porownania")
+{
+    double tab1[3]={1,2,3};
+    Vector3D wektor1(tab1);
+    Vector3D wektor2(tab1);
+
+    CHECK(wektor1 == wektor2);
+}
+
+TEST_CASE("operator porownania")
+{
+    double tab1[3]={1,2,3};
+    Vector3D wektor1(tab1);
+    Vector3D wektor2(tab1);
+
+    CHECK(wektor1 == wektor2);
+}
+
+// TEST_CASE("mierzenie odległosci")
+// {
+//     double tab1[3]={1,2,3};
+//     double wynik;
+//     Vector3D wektor1(tab1);
+//     Vector3D wektor2(tab1);
+
+//     wynik = wektor1.spr(wektor2) ;
+
+//     CHECK(wynik == 0);
+// }
+
+
